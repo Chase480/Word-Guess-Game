ffc75b92cdf26ee5e5d16b87aad1fee95126d8a5
Chase Perez <chaseperez480@gmail.com>
2021-04-13 01:10:57 -0700

    created variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,45 @@
-const guessedLetters = [];
+const gameCharactors = ["wayne", "stewart", "jonesy", "roald", "mcmurray", "katy", "gail", "squirrelly dan", "pastor glen", "reilly"];
 
-const guessesLeft = 12;
+var guessedLetters = [];
 
-const wins = 0;
-const losses = 0;
+var wordToGuess = null;
+
+var guessesLeft = 10;
+
+var wins = 0;
+var losses = 0;
+
+var updateWordToGuess = function () {
+    wordToGuess = gameCharactors[Math.floor(Math.random() * gameCharactors.length)];
+};
+
+var updateGuessedLetters = function () {
+    document.querySelector("#guesses-so-far").innerHTML = guessedLetters.join(", ");
+};
+
+var updateGuessesLeft = function () {
+    document.querySelector("#guesses-left").innerHTML = guessesLeft;
+};
+
+const reset = function () {
+    guessedLetters = [];
+    guessesLeft = 10;
+    updateGuessedLetters();
+    updateGuessesLeft();
+    updateWordToGuess();
+};
+
+updateGuessesLeft();
+updateWordToGuess();
+
+document.onkeyup = function (event) {
+
+    guessesLeft--;
+
+    const letter = event.key.toLocaleUpperCase();
+
+    guessedLetters.push(letter);
+
+    updateGuessedLetters();
+    updateGuessesLeft();
+};
